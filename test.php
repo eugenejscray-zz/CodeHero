88cd6f6c37e9cf9c42ae75d07fc6c7f22a8af749
eugene.j.scray@gmail.com <eugene.j.scray@gmail.com>
2015-01-07 13:39:27 -0800

    updating

---
+++ test.php
@@ -0,0 +1,39 @@
+<?php
+// get each word line by line.
+$fileArray = file($argv[1]);
+$index = 0;
+$linecount = 0;
+
+foreach($fileArray as $line){
+	// does not compare previous items since its not neccessary.
+	for($i = $index + 1; $i < count($fileArray); $i++){
+		if(isAnagram($line, $fileArray[$i])){
+			// we are comaring the item to itself
+			echo $line . " is an anagram of " . $fileArray[$i];
+		}
+	}
+	++$index;
+}
+
+
+// compares the two inputs and outputs true/false if the two are anagrams.
+function isAnagram($word_one, $word_two){
+	
+	// take the first word and convert into an array
+	$array1 = str_split($word_one);
+	$array2 = str_split($word_two);
+
+	// sort the arrays
+	sort($array1);
+	sort($array2);
+	
+	// compare the sorted arrays.
+	if($array1 === $array2)
+		return true;
+	return false;
+}
+
+
+
+
+?>
